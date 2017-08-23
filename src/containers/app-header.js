581996f7aef89d59041fa82a603578dbf6deab5f
Tarun <tarun.softengg@gmail.com>
2017-08-23 10:14:17 +0530

    state added to header component

--- src/containers/app-header.js
+++ src/containers/app-header.js
@@ -3,12 +3,27 @@ import { connect } from 'react-redux';
 import * as Action from '../actions';
 
 class AppHeader extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filter : {
+        'company' :'',
+         'location' : ''
+      },
+      applyFilter : false
+    }
+    this.toggleFilters = this.toggleFilters.bind(this);
+  }
+  toggleFilters(){
+    this.setState({'applyFilter' : ! this.state.applyFilter});
+  }
   render() {
     return (
       <div className="app-header">
         <div className="filters">
         <h3>Candidate Search Filters <span className="applied-filters">applied filters ()</span></h3>
-        <a className="filter-action">+ add filters</a>
+        <a onClick={this.toggleFilters} className="filter-action">+ add filters</a>
+          { this.state.applyFilter ? (<input type="text" className="field-control"/> ) : '' }
         </div>
         <div className="search">
            <input className="field-control" type="text" />
