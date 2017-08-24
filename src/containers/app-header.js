5cad5ac3924e424a89c7395158384f28af183c26
Tarun <tarun.softengg@gmail.com>
2017-08-24 11:37:33 +0530

    container comp local state object created

--- src/containers/app-header.js
+++ src/containers/app-header.js
@@ -6,27 +6,71 @@ class AppHeader extends Component {
   constructor(props){
     super(props);
     this.state = {
-      filter : {
+      'filter' : {
         'company' :'',
-         'location' : ''
+         'location' : '',
+         'name' : ''
       },
-      applyFilter : false
+      'applyFilter' : true
     }
     this.toggleFilters = this.toggleFilters.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   toggleFilters(){
     this.setState({'applyFilter' : ! this.state.applyFilter});
+     // if applyFilter is false than remove filters from api
+    if(this.state.applyFilter){
+      this.setState({'filter' : {} });
+      //this.props.getDashboardData();
+    }
   }
+  handleChange(e){
+    console.log(e.target.value);
+    switch(e.target.id){
+      case 'name' :
+      this.state.filter = {'company' : this.state.filter.company, 'location' : this.state.filter.location,
+      'name' : e.target.value};
+      break;
+
+      case 'location' :
+      this.state.filter = {'company' : this.state.filter.company, 'location' : e.target.value ,'name' : this.state.filter.name};
+      break;
+
+      case 'company' :
+      this.state.filter = {'location' : this.state.filter.location, 'company' : e.target.value ,'name' : this.state.filter.name};
+      break;
+    }
+    this.setState({'filter' : this.state.filter });
+    //this.props.getDashboardData(this.state.filter);
+  }
+
   render() {
     return (
       <div className="app-header">
         <div className="filters">
         <h3>Candidate Search Filters <span className="applied-filters">applied filters ()</span></h3>
-        <a onClick={this.toggleFilters} className="filter-action">+ add filters</a>
-          { this.state.applyFilter ? (<input type="text" className="field-control"/> ) : '' }
+        <h5 className="filter-label">showing candidate from {this.state.filter.company} {this.state.filter.location}</h5>
+        <span onClick={this.toggleFilters} className="filter-action">
+        {this.state.applyFilter ? 'Remove Filters' : 'Add Filters' }</span>
+          { this.state.applyFilter ? (
+          <div className="select-filters">
+              <select onChange={this.handleChange} className="field-control" id="location">
+              <option>loc1</option>
+              <option>loc2</option>
+              <option>loc3</option>
+
+              </select>
+               <select onChange={this.handleChange}  className="field-control" id="company">
+               <option>coc1</option>
+               <option>coc2</option>
+               <option>coc3</option>
+              </select>
+           </div>
+
+          ) : '' }
         </div>
         <div className="search">
-           <input className="field-control" type="text" />
+           <input placeholder="sort by name" id="name" className="field-control" type="text" onChange={this.handleChange} />
         </div>
       </div>
     );
